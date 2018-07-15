8a95181cee8f8caa77ff03b7870eac2e68d450ff
Nikee <rivera.nikee@gmail.com>
2018-07-15 15:17:52 -0400

    added all functions

--- liri.js
+++ liri.js
@@ -20,7 +20,7 @@ var client = new Twitter(keys.twitter);
 var arguments = process.argv;
 var command = arguments[2];
 
-// Input for movie, song.
+// Input for movie, song. In order to work properly put replace spaces with a '+'.
 var input = arguments[3];
 
 // ===================== Functions ===================== //
@@ -41,7 +41,11 @@ switch (command) {
     break;
 
     case 'movie-this':
-        movie(input);
+    if (input) {
+        movies(input)
+    } else {
+        movies('Mr. Nobody');
+    }
     break;
 
     case 'do-what-it-says':
@@ -102,3 +106,65 @@ function spotify(input) {
     });
 }
 
+// This controls the movie search.
+
+function movies(input) {
+    var omdbURL = 'http://www.omdbapi.com/?t=' + input + '&apikey=trilogy&'
+    
+    request(omdbURL, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var body = JSON.parse(body);
+
+            console.log('Title: ' + body.Title);
+            console.log('Release Year: ' + body.Year);
+            console.log('IMDB Rating: ' + body.imdbRating);
+            console.log('Rotten Tomatoes Rating: ' + body.Ratings[1].Value);
+            console.log('Country Produced: ' + body.Country);
+            console.log('Language: ' + body.Language);
+            console.log('Plot: ' + body.Plot);
+            console.log('Actors: ' + body.Actors);
+
+        // This adds info to log.txt.
+        fs.appendFile('log.txt', 'Title: ' + body.Title);
+        fs.appendFile('log.txt', 'Release Year: ' + body.Year);
+        fs.appendFile('log.txt', 'IMDB Rating: ' + body.imdbRating);
+        fs.appendFile('log.txt', 'Rotten Tomatoes Rating: ' + body.Ratings[1].Value);
+        fs.appendFile('log.txt', 'Country Produced: ' +body.Country);
+        fs.appendFile('log.txt', 'Language: ' + body.Language);
+        fs.appendFile('log.txt', 'Plot: ' + body.Plot);
+        fs.appendFile('log.txt', 'Actors: ' + body.Actors);
+        } else {
+            console.log(error);
+        }
+
+        if (movies === 'Mr. Nobody') {
+            console.log('------------------------');
+            console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+            console.log("It's on Netflix!");
+
+        // This adds info to log.txt.
+            fs.appendFile('log.txt', '------------------------');
+            fs.appendFile('log.txt', "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+            fs.appendFile('log.txt', "It's on Netflix!");
+        }
+    });
+}
+
+// Controls the Do What it Says command.
+
+function doIt() {
+    fs.readFile('random.txt', 'utf8', function(error, data) {
+        if (error) {
+            console.log(error);
+        }
+    
+    // This will make it readable.
+    var txt = data.split(',');
+
+    // These are inputs for each command while running in random.txt.
+    spotify(txt[1]);
+    tweet(txt[2]);
+    movies(txt[3]);
+    doIt(txt[4]);
+    });
+}
